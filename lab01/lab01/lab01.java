523495cf4b8ab9a86e7f2ce7fa7951f1f1420865
shanerego <35306988+shanerego@users.noreply.github.com>
2018-01-16 12:20:15 -0500

    second commit

---
+++ lab01/lab01/lab01.java
@@ -0,0 +1,9 @@
+package lab01;
+
+public class lab01 {
+
+	public static void main() {
+		print ("hello world")
+	}
+
+}
---
+++ lab01/lab01/src/lab01/lab01.java
@@ -0,0 +1,10 @@
+package lab01;
+
+public class lab01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
